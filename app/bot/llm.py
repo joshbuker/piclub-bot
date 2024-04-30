fd2d583de63a8d2e2c2b79248039c11862c28cd7
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-04-30 14:54:35 -0700

    Use async http package for llm.generate_response

--- app/bot/llm.py
+++ app/bot/llm.py
@@ -3,11 +3,7 @@ Integration with LLMs
 """
 
 import json
-import requests
-from requests.exceptions import (
-    HTTPError,
-    ConnectionError,
-)
+import aiohttp
 
 import globalconf as _globalconf
 from . import botconf as _botconf
@@ -17,73 +13,84 @@ import discord
 async def generate_response(
     message: discord.Message,
     system_prompt: str = _botconf.botconfig.system_prompt,
-    auto_pull_model: bool = _botconf.botconfig.auto_pull_model
+    auto_pull_model: bool = _botconf.botconfig.auto_pull_model # TODO: Use this again
 ) -> str | None:
+    url = f"http://{_globalconf.LLM_HOST}:{_globalconf.LLM_PORT}/api/generate"
+    print(f"url: {url}")
+
     # While receiving responses, show typing status
     async with message.channel.typing():
-
         print(f"Generating response ...\nUser prompt:\n{message.content}\nSystem prompt:\n{system_prompt}")
 
-        url = f"http://{_globalconf.LLM_HOST}:{_globalconf.LLM_PORT}/api/generate"
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post(url, json={
+                "model": _botconf.botconfig.llm_model,
+                "prompt": message.content,
+                "system": system_prompt,
+                "context": [],
+                "stream": False,
+            }) as res:
+                try:
+                    data = await res.json()
+                    if "error" in data:
+                        print(f"Error: {data['error']}")
+                        print(f"data: {json.dumps(data)}")
+                        return None
 
-        try:
-            r = requests.post(
-                url,
-                json={
-                    "model": _botconf.botconfig.llm_model,
-                    "prompt": message.content,
-                    "system": system_prompt,
-                    "context": [],
-                },
-                stream=True,
-            )
+                    dur = data["total_duration"] / 1_000_000_000
+                    print(f"response took {dur:.3f} seconds")
 
-            r.raise_for_status()
-        except HTTPError as e:
-            err_res = e.response
-            if not isinstance(err_res, requests.models.Response):
-                raise e
-
-            print(f"Error:\n{err_res.json()}")
-
-            if err_res.status_code == 404 and auto_pull_model:
-                pull_model(_botconf.botconfig.llm_model)
-
-            return None
-        except ConnectionError as e:
-            print(f"Ollama server unavailable at {url}")
-            return None
-
-        response = ""
-            # Loop through tokens as they are streamed in
-        for line in r.iter_lines():
-            body = json.loads(line) # Parse response as json
-
-            # Append token to response
-            response_part = body.get("response", "")
-            response += response_part
-
-            # Raise error if present
-            if "error" in body:
-                raise Exception(body["error"])
-
-            # Return response if done
-            if body.get("done", False):
-                return response
-
-        # Return response
-        return None
+                    return data["response"]
 
 
-def pull_model(model: str):
-    print(f"Pulling model: {model} ...")
-    r = requests.post(
-        f"http://{_globalconf.LLM_HOST}:{_globalconf.LLM_PORT}/api/pull",
-        json={
-            "name": model,
-            "stream": False,
-        },
-    )
+                except Exception as e:
+                    #err_res = e.response
+                    #if not isinstance(err_res, requests.models.Response):
+                        #raise e
 
-    r.raise_for_status()
-    print(r.json())
+                    print(f"Error:\n{e}\nType: {type(e)}")
+
+                    #if err_res.status_code == 404 and auto_pull_model:
+                        #pull_model(_botconf.botconfig.llm_model)
+
+                    return None
+                #except ConnectionError as e:
+                    #print(f"Ollama server unavailable at {url}")
+                    #return None
+
+        #response = ""
+            ## Loop through tokens as they are streamed in
+        #for line in r.iter_lines():
+            #body = json.loads(line) # Parse response as json
+
+            ## Append token to response
+            #response_part = body.get("response", "")
+            #response += response_part
+
+            ## Raise error if present
+            #if "error" in body:
+                #raise Exception(body["error"])
+
+            ## Return response if done
+            #if body.get("done", False):
+                #print("Done typing (done)")
+                #return response
+
+        #print("Done typing (no more lines)")
+
+        ## Return response
+        #return None
+
+
+#def pull_model(model: str):
+    #print(f"Pulling model: {model} ...")
+    #r = requests.post(
+        #f"http://{_globalconf.LLM_HOST}:{_globalconf.LLM_PORT}/api/pull",
+        #json={
+            #"name": model,
+            #"stream": False,
+        #},
+    #)
+
+    #r.raise_for_status()
+    #print(r.json())
